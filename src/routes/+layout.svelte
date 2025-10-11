9e03c503331f10604caa5d83e3d97617af55d900
MarshalZhukov123 <marshalzhukov420@gmail.com>
2025-10-11 23:02:36 +0000

    tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,29 +1,36 @@
 <script>
-	
 	import '../app.css';
 
 	/** @type {{children: import('svelte').Snippet}} */
 	let { children } = $props();
 </script>
 
-<div class="min-h-screen flex flex-col">
-
-	<main class="bg-gradient-to-b from-secondary via-base-300 to-base-200 flex-grow">
+<div class="flex min-h-screen flex-col">
+	<main class="flex-grow bg-gradient-to-b from-secondary via-base-300 to-base-200">
 		{@render children()}
 	</main>
 	<div>
+		<hr />
 
-	<hr/>
+		<div class="flex justify-around bg-gradient-to-br from-base-content via-neutral to-primary">
+			<a class="btn m-2 rounded-xl border-accent btn-secondary" href="/">Home</a>
+			<a class="btn m-2 rounded-xl border-accent btn-secondary hover:btn-error" href="/pomodoro"
+				>Pomodoro</a
+			>
+			<a class="btn m-2 rounded-xl border-accent btn-secondary hover:btn-warning" href="/tasks"
+				>Task Checklist</a
+			>
+			<a class="btn m-2 rounded-xl border-accent btn-secondary hover:btn-success" href="/habits"
+				>Habit Builder</a
+			>
+			<a class="btn m-2 rounded-xl border-accent btn-secondary hover:bg-yellow-400" href="/tags"
+				>Tags</a
+			>
+			<a class="btn m-2 rounded-xl border-accent btn-secondary hover:bg-yellow-400" href="/stats"
+				>All-Time Stats</a
+			>
+		</div>
 
-	<div class="bg-gradient-to-br from-base-content via-neutral to-primary flex justify-around">
-		<a class="btn btn-secondary m-2 border-accent rounded-xl" href="/">Home</a>
-		<a class="btn btn-secondary m-2 border-accent rounded-xl hover:btn-error" href="/pomodoro">Pomodoro</a>
-		<a class="btn btn-secondary m-2 border-accent rounded-xl hover:btn-warning" href="/tasks">Task Checklist</a>
-		<a class="btn btn-secondary m-2 border-accent rounded-xl hover:btn-success" href="/habits">Habit Builder</a>
-		<a class="btn btn-secondary border-accent rounded-xl m-2 hover:bg-yellow-400" href="/stats">All-Time Stats</a>
-	</div>
-
-	<hr/>
-	
+		<hr />
 	</div>
 </div>
---
+++ src/routes/tags/+page.svelte
@@ -0,0 +1,79 @@
+<script>
+	let tags = $state([
+		{ name: 'Tag 1', deleted: false, id: 1, creationDate: '' },
+		{ name: 'Tag 2', deleted: false, id: 2, creationDate: '' },
+		{ name: 'Tag 3', deleted: false, id: 3, creationDate: '' }
+	]);
+	let kepttags = $derived(tags.filter((tag) => tag.deleted == false));
+	let inputTag = $state('');
+	const today = new Date();
+	const formattedToday = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+	function addTag() {
+		tags.push({ name: inputTag, deleted: false, id: Date.now(), creationDate: formattedToday });
+		inputTag = '';
+	}
+	function deleteTag(id) {
+		// if (!confirm('Are you sure?')) return;
+		for (let i = 0; i < tags.length; i++) {
+			if (tags[i].id === id) {
+				tags[i].deleted = true;
+			}
+		}
+	}
+	let sortKey = $state('az');
+
+	let sortedList1 = $derived([...tags].sort((a, b) => a.name.localeCompare(b.name)));
+
+	let sortedList2 = $derived(
+		[...tags].sort((b, a) => a.creationDate.localeCompare(b.creationDate))
+	);
+
+	let sortedList3 = $derived([...tags].sort((a) => a.creationDate.localeCompare(b.creationDate)));
+
+	let sortedList = $derived(
+		sortKey == 'az' ? sortedList1 : sortKey == 'date' ? sortedList2 : sortedList3
+	);
+</script>
+
+<div
+	class="faustina flex justify-center bg-gradient-to-br from-info to-purple-900/50 p-3 text-6xl text-black"
+>
+	Tags
+</div>
+<hr />
+<div class="bg-gradient-to-br from-base-content via-neutral to-primary">
+	<input
+		class="m-2 rounded-xl border bg-purple-300/50 p-2 text-black focus:ring-2 focus:ring-primary focus:outline-none"
+		placeholder="Add a new tag..."
+		bind:value={inputTag}
+	/>
+
+	<button
+		class="m-2 rounded-xl border bg-purple-300/50 p-2 font-semibold hover:bg-purple-200/70 disabled:text-gray-500"
+		onclick={addTag}
+		disabled={inputTag.length == 0}>Add Tag</button
+	>
+	<select
+		bind:value={sortKey}
+		class="select m-2 rounded-xl border bg-purple-300/50 p-2 text-black select-secondary focus:ring-2 focus:ring-primary focus:outline-none"
+	>
+		<option value={''} disabled selected>Sort Tags</option>
+		<option value={'az'}>Alphabetically (A-Z)</option>
+		<option value={'date'}>Date Created (newest)</option>
+		<option value={'date2'}>Date Created (oldest)</option>
+	</select>
+</div>
+<hr />
+<div class="flex flex-wrap">
+	{#each sortedList as tag}
+		<div class="m-0.5 p-1">
+			<div class=" m-0.5 rounded-xl border bg-info p-1 font-semibold">
+				{tag.name}
+				<button
+					class="m-1 rounded-xl border bg-error p-1 text-sm font-semibold hover:bg-red-300"
+					onclick={() => deleteTag(tag.id)}>❌</button
+				>
+			</div>
+		</div>
+	{/each}
+</div>
